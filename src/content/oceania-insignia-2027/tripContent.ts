4bf77c6186fc6a14b18fbae47f6d34b94c1a781f
niryoav <niryoav@hotmail.com>
2026-09-20 16:58:55 +0200

    feat: add 2027 trip content shell

---
+++ src/content/oceania-insignia-2027/tripContent.ts
@@ -0,0 +1,9 @@
+import type { TripContentBundle } from '../../domain/content/contentTypes'
+
+export const oceaniaInsignia2027TripContent: TripContentBundle = {
+  schemaVersion: 1,
+  contentVersion: '2026-09-20.1',
+  tripId: 'trip-oceania-insignia-2027',
+  destinationGuides: [],
+  excursionGuides: [],
+}
